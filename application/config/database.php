dd651977ad7f404675212c44ab07c6b2e853b849
HaiCuCai-00 <69022895+HaiCuCai-00@users.noreply.github.com>
2021-05-13 01:13:36 +0700

    Update database.php

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'shopbanhang',
+	'hostname' => 'sql6.freesqldatabase.com',
+	'username' => 'sql6412008',
+	'password' => 'QkXYVqimU8',
+	'database' => 'sql6412008',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
